ee0425c5613d7aa38fb00b2455b4704bbdba3d38
Marcelo <marceloyk@gmail.com>
2024-05-22 15:19:30 -0400

    Exercício 23: Alterar área

---
+++ backend/src/intranet_trems/src/intranet_trems/upgrades/v20240522002.py
@@ -0,0 +1,16 @@
+from intranet_trems import logger
+from plone import api
+
+
+def alterar_permissionamento_estrutura(context):
+    portal = api.portal.get()
+    estrutura = portal["estrutura"]
+    permission_id = "intranet_trems: Add Area"
+    roles = [
+        "Contributor",
+        "Editor",
+        "Manager",
+        "Site Administrator",
+    ]
+    estrutura.manage_permission(permission_id, roles=roles)
+    logger.info(f"Alterado permissionamento em {estrutura}.")
