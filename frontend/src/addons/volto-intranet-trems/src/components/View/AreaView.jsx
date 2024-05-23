07fdf3795ff3367c3f531f494be736fb0f6ea8cb
Marcelo <marceloyk@gmail.com>
2024-05-23 14:19:23 -0400

    Exercício 39: visão Área - outros campos

--- frontend/src/addons/volto-intranet-trems/src/components/View/AreaView.jsx
+++ frontend/src/addons/volto-intranet-trems/src/components/View/AreaView.jsx
@@ -2,12 +2,14 @@
  * AreaView view component.
  * @module components/View/AreaView
  */
-
 import React from 'react';
 import PropTypes from 'prop-types';
+import { List, Table } from 'semantic-ui-react';
+import { Icon, UniversalLink } from '@plone/volto/components';
+import houseSVG from '@plone/volto/icons/home.svg';
 
 /**
- * AreaView view component class.
+ * AreaView view component.
  * @function AreaView
  * @params {object} content Content object.
  * @returns {string} Markup of the component.
@@ -23,6 +25,64 @@ const AreaView = (props) => {
       <div>
         <p className="description documentDescription">{content.description}</p>
       </div>
+      <div>
+        <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell singleLine>Contato</Table.HeaderCell>
+              <Table.HeaderCell></Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Tipo de E-mail</Table.Cell>
+              <Table.Cell singleLine>
+                {content.tipo_email ? (
+                  <span>{content.tipo_email.title}</span>
+                ) : (
+                  ''
+                )}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>E-mail</Table.Cell>
+              <Table.Cell singleLine>
+                {content.email ? (
+                  <a href={`mailto: ${content.email}`}>{content.email}</a>
+                ) : (
+                  ''
+                )}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>Ramal</Table.Cell>
+              <Table.Cell singleLine>
+                {content.ramal ? content.ramal : '-'}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+      <List className={'subareas'}>
+        {content.items &&
+          content.items.map(function (area, i) {
+            return (
+              <List.Item key={i}>
+                <div className={'icon'}>
+                  <Icon name={houseSVG} size="24px" />
+                </div>
+                <List.Content>
+                  <List.Header>
+                    <UniversalLink href={area['@id']}>
+                      {area.title}
+                    </UniversalLink>
+                  </List.Header>
+                  <List.Description>{area.description}</List.Description>
+                </List.Content>
+              </List.Item>
+            );
+          })}
+      </List>
     </div>
   );
 };
